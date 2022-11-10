c72d47811bbc1040c6fc813995115952b6af7d53
MargaretBassney <mbassney@smith.edu>
2022-11-10 09:06:43 -0500

    adding description of data

--- R/datasets.R
+++ R/datasets.R
@@ -1,1 +1,18 @@
-
+#' Unisex Names Data
+#'
+#' Data that contains 900 names given to each gender at least
+#'one third of the time with a minimum of 100 people with that name.
+#'
+#' @format ## `unisex`
+#' A data frame with 919 rows and 6 columns:
+#' \describe{
+#'   \item{x1}{index}
+#'   \item{name}{first names from the Social Security Administration}
+#'   \item{total}{total number of people with that name}
+#'   \item{male_share}{percentage of males with that name}
+#'   \item{female_share}{percentage of females with that name}
+#'   \item{gap}{difference in percentage of males and females with a certain name}
+#'   ...
+#' }
+#' @source <https://data.world/fivethirtyeight/unisex-names>
+"unisex"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,6 @@
-## code to prepare `DATASET` dataset goes here
-
-usethis::use_data(DATASET, overwrite = TRUE)
+## code to prepare `unisex` dataset goes here
+library(tidyverse)
+library(janitor)
+unisex<- read_csv("data-raw/unisex")%>%
+  clean_names()
+usethis::use_data(unisex, overwrite = TRUE)
