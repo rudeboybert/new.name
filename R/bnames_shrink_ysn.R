46b934bd1d0811d76d9a58fb827e6ef35970ab2b
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-11-10 18:28:56 -0500

    Added roxygen details to bnames_shrink function.

--- R/bnames_shrink_ysn.R
+++ R/bnames_shrink_ysn.R
@@ -1,7 +1,14 @@
-bnames_shrink_ysn <- function(x) {
-  bnames_shrink_ysn <- babynames::babynames |>
+#' Shrink Babynames
+#' Filters Babynames dataframe to year/sex/name columns only
+#' @return dataframe
+#' @export
+#' @import babynames
+#' @examples
+#' bnames_shrink_ysn()
+bnames_shrink_ysn <- function() {
+  bnames_shrink_ysn_df <- babynames::babynames |>
     select(year, sex, name) |>
     as.data.frame()
+  return()
 }
-
 
