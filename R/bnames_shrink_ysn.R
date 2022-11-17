0ceddb27782dc604757c89eac7a20b10482f2a8a
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-11-17 09:47:18 -0500

    Update main function

--- R/bnames_shrink_ysn.R
+++ R/bnames_shrink_ysn.R
@@ -1,14 +1,21 @@
-#' Shrink Babynames:
-#' Filters Babynames dataframe to year/sex/name columns only
-#' @return dataframe
+#' New Name:
+#' Returns three random baby names that are from a given year between 1880-2021
+#' and also start with the first letter of a user-inputted name.
+#' @return list
 #' @export
 #' @import babynames
 #' @examples
-#' bnames_shrink_ysn()
-bnames_shrink_ysn <- function() {
-  bnames_shrink_ysn_df <- babynames::babynames |>
-    select(year, sex, name) |>
-    as.data.frame()
-  return()
+#' new.name()
+new.names <- function() {
+  target_a = "^" #to be combined with target_n for regEx in target_x
+  target_n = substr(x,1,1) #targets first character of input name
+  target_x = paste(target_a, target_n, sep = "")
+  names <- babynames |>
+    select(name, year) |>
+    filter(year == y) #shrinks babynames dataframe to name/year columns and filter by inputted year
+  target <- names |>
+    filter(str_detect(name, target_x)) #filter dataframe to to names that start with target letter
+  target_rand <- target[sample(1:nrow(target), 3),]
+  target_ret = list(target_rand$name)
+  return(target_ret)
 }
-
