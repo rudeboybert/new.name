993002a19356bb245885e22ec4e6c3a1be42198a
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-11-17 09:53:08 -0500

    Changed Roxygen.

--- R/bnames_shrink_ysn.R
+++ R/bnames_shrink_ysn.R
@@ -1,12 +1,14 @@
-#' New Name:
-#' Returns three random baby names that are from a given year between 1880-2021
+#' New Name: Returns three random baby names that are from a given year between 1880-2021
 #' and also start with the first letter of a user-inputted name.
+#'
+#' @param x Name, character
+#' @param y Year, int
+#'
 #' @return list
 #' @export
 #' @import babynames
 #' @examples
-#' new.name()
-new.names <- function() {
+newNames <- function(x,y) {
   target_a = "^" #to be combined with target_n for regEx in target_x
   target_n = substr(x,1,1) #targets first character of input name
   target_x = paste(target_a, target_n, sep = "")
