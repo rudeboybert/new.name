74e755cb54b4d058e320094801df29e91d915885
Mars Ikeda <89821623+mars-ikeda@users.noreply.github.com>
2022-11-17 09:55:29 -0500

    added dplyr back to roxygen code

--- R/bnames_shrink_ysn.R
+++ R/bnames_shrink_ysn.R
@@ -6,7 +6,7 @@
 #'
 #' @return list
 #' @export
-#' @import babynames
+#' @import babynames, dplyr
 #' @examples
 newNames <- function(x,y) {
   target_a = "^" #to be combined with target_n for regEx in target_x
